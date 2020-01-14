eac9382cd7967d9d322d78e2f122823b1467a499
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-01-14 13:57:14 +0200

    start using dotenv to set enviroment variables

--- TurkuMemoriesBackend/app.js
+++ TurkuMemoriesBackend/app.js
@@ -1,13 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const env = require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+if (env.error) {
+  console.error('FATAL ERROR: .env file is not defined');
+  process.exit(1);
+}
+
+// database setup here?
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
