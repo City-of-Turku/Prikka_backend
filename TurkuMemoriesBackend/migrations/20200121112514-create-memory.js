02b47d82a7954eeefbce19196f5a0de20cff5608
Haider <haider.alkaleedy@edu.turkuamk.fi>
2020-01-21 14:22:25 +0200

    Updated models and migrations

--- TurkuMemoriesBackend/migrations/20200121112514-create-memory.js
+++ TurkuMemoriesBackend/migrations/20200121112514-create-memory.js
@@ -2,22 +2,35 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Memories', {
-      id: {
+      Title: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      MemoryId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      title: {
+      MemoryContent: {
+        allowNull: false,
         type: Sequelize.TEXT
       },
-      createdAt: {
+      MemoryDate: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+      MemoryPublished: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      MemoryLatitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      MemoryLongtitude: {
+        type: Sequelize.FLOAT,
+        allowNull: false
       }
     });
   },
--- TurkuMemoriesBackend/models/memory.js
+++ TurkuMemoriesBackend/models/memory.js
@@ -1,10 +1,59 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Memory = sequelize.define('Memory', {
-    title: DataTypes.TEXT
-  }, {});
+    Title: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    MemoryId: {
+      type: DataTypes.INT,
+      allowNull: false
+    },
+    CategoryId: {
+      type: DataTypes.INT,
+      allowNull: false
+    },
+    MemoryContent: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+          min: {
+              args: 4,
+              msg: "Content has to be minimum 4 characters"
+          }
+          }
+  },
+  MemoryDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+  },
+  MemoryPublished: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW
+  },
+  MemoryLatitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+  },
+  MemoryLongtitude: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+  }
+
+
+
+  });
   Memory.associate = function(models) {
     // associations can be defined here
+    Memory.belongsTo(models.User,{
+      foreignkey: 'userId',
+      as: 'user',
+    });
+    /*Memory.hasOne(models.Category,{
+      foreignkey: 'categoryId',
+      as: 'category',
+    });*/
+
   };
   return Memory;
 };
