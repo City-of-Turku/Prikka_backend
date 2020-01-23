99cd53c2020b6c44360aa3c6aafbd8e4ea0dddd8
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-01-23 13:08:53 +0200

    some out-commented code for making users into db

--- TurkuMemoriesBackend/routes/login.js
+++ TurkuMemoriesBackend/routes/login.js
@@ -11,12 +11,18 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const router = express.Router();
 
+// const User = require('../models/user');
+
 passport.use(new GoogleStrategy({
     clientID: process.env['GOOGLE_CLIENT_ID'],
     clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
     callbackURL: '/api/login/google-return'
     },
     function (accessToken, refreshToken, profile, cb) {
+        // User.findOrCreate({googleId: profile.id}, function (err, user) {
+            // return cb(err, user);
+        //})        
+
         return cb(null, profile);
     }
     ));
