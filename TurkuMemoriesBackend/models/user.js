f28b06cc9833cf00297175b7d7fe23e2f8050c58
Matti <matti.hietikko@edu.turkuamk.fi>
2020-01-28 09:46:43 +0200

    Corrected memoryId -> id in models

--- TurkuMemoriesBackend/models/user.js
+++ TurkuMemoriesBackend/models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.Memory,{
-        foreignKey: 'memoryId',
+        foreignKey: 'id',
         as: 'memories',
     });
   };
