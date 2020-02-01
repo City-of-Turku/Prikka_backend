223551ac43cd32717a239cc49448fee99244e862
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-02-01 19:11:25 +0200

    move google/fb strategies to auth.js conf, try to fix login, shit's fucked yo

--- TurkuMemoriesBackend/config/auth.js
+++ TurkuMemoriesBackend/config/auth.js
@@ -1,20 +1,72 @@
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/user.js');
 const bcrypt = require('bcryptjs');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+const User = require('../models/user.js');
+const Models = require('../models');
+
 // function to be called while there is a new sign/signup 
 // We are using passport local signin/signup strategies for our app
 
 module.exports = (passport) => {
 
-    passport.serializeUser((user, next) => {
-        next(null, user);
+    passport.serializeUser((user, done) => {
+        console.log("USER SER.", user.dataValues.id)
+        done(null, user.id);
     });
 
-    passport.deserializeUser((id, next) => {
-        User.findById(id, (err, user) => {
-            next(err, user);
+    passport.deserializeUser((id, done) => {
+        Models.findByPk(id, (err, user) => {
+            done(err, user);
         })
     });
+
+    passport.use(
+        new GoogleStrategy(
+            // strat settings
+            {
+                clientID: process.env['GOOGLE_CLIENT_ID'],
+                clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+                callbackURL: '/api/login/google-return',
+            },
+            function(accessToken, refreshToken, profile, done) {
+                // passport callback function
+                console.log(profile)
+                process.nextTick(() => {
+                    Models.User
+                        .findOrCreate({
+                            where: { googleId: profile.id },
+                            defaults: {
+                                googleId: profile.id,
+                                name: profile.displayName,
+                                email: 'asdo@asd.fi',
+                                passwordhash: 'asdasda'
+                            }
+                        })
+                        .then(([user, created, error]) => {
+                            console.log("then")
+                            console.log("USER",user.dataValues)
+                            done(null, user);
+                        });
+                })
+            }
+        )
+    );
+    
+    passport.use(
+        new FacebookStrategy(
+            {
+                clientID: process.env['FACEBOOK_APP_ID'],
+                clientSecret: process.env['FACEBOOK_APP_SECRET'],
+                callbackURL: '/api/login/facebook-return'
+            },
+            function(accessToken, refreshToken, profile, cb) {
+                return cb(null, profile);
+            }
+        )
+    );
+
 
     const localRegister = new LocalStrategy({
         usernameField: 'email',
--- TurkuMemoriesBackend/routes/login.js
+++ TurkuMemoriesBackend/routes/login.js
@@ -7,96 +7,10 @@
  */
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/api/login');
 const router = express.Router();
 
-const Models = require('../models');
 
-//var values = { slack_id: profile.id, name: profile.user };
-//var selector = { where: { slack_id: profile.id } };
-
-// User.findOrCreate(selector, values)
-//         .then(function() {
-//             return done(err, user);
-//         });
-
-// console.log(Models.User.findOrCreate());
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env['GOOGLE_CLIENT_ID'],
-			clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-			callbackURL: '/api/login/google-return',
-			passReqToCallback: true
-		},
-		function(request, accessToken, refreshToken, profile, cb) {
-            process.nextTick(() => {
-                Models.User
-                    .findOrCreate({
-                        where: { googleId: profile.id },
-                        defaults: {
-                            googleId: profile.id,
-                            name: profile.displayName,
-                            email: 'asdo@asd.fi',
-                            passwordhash: 'asdasda'
-                        }
-                    })
-                    .then(([user, created, error]) => {
-                        console.log(created)
-                        console.log(user)
-                        return cb(error, user.dataValues.googleId);
-                    });
-            })
-		}
-	)
-);
-
-// passport.use(new GoogleStrategy({
-//     clientID: process.env['GOOGLE_CLIENT_ID'],
-//     clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-//     callbackURL: '/api/login/google-return'
-//     },
-//     function (accessToken, refreshToken, profile, cb) {
-//         Models.User
-//         .then(function (err, user) {
-//             return cb(err, user);
-//         });
-
-//         return cb(null, profile);
-//     }
-//     ));
-
-passport.use(
-	new FacebookStrategy(
-		{
-			clientID: process.env['FACEBOOK_APP_ID'],
-			clientSecret: process.env['FACEBOOK_APP_SECRET'],
-			callbackURL: '/api/login/facebook-return'
-		},
-		function(accessToken, refreshToken, profile, cb) {
-			return cb(null, profile);
-		}
-	)
-);
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
-passport.serializeUser(function(user, cb) {
-	cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-	cb(null, obj);
-});
 
 // login page
 router.get('/', function(req, res) {
@@ -110,7 +24,7 @@ router.get('/google-return', passport.authenticate('google', { failureRedirect: 
 });
 
 router.get( '/google-return',
-	passport.authenticate( 'google', {
+	passport.authenticate('google', {
 		successRedirect: '/profile',
 		failureRedirect: '/error'
 }));
