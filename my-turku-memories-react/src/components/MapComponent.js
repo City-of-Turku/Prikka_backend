b9f7a78313bd1fa29ae13060aa86cbbdc2d63336
Petteri Peltokangas <petteri.peltokangas@edu.turkuamk.fi>
2020-02-07 10:41:47 +0200

    Add example marker on map

--- my-turku-memories-react/src/components/MapComponent.js
+++ my-turku-memories-react/src/components/MapComponent.js
@@ -1,20 +1,27 @@
 //https://blog.logrocket.com/how-to-use-react-leaflet/
 //https://www.azavea.com/blog/2016/12/05/getting-started-with-react-and-leaflet/
+//https://cherniavskii.com/using-leaflet-in-react-apps/
 import React, { Component } from 'react'
-import { Map, TileLayer } from 'react-leaflet'
+//import { Map, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
 
 export class MapComponent extends Component {
-  state = {
-    center: [60.45, 22.26],
+  componentDidMount() {
+    this.map = L.map('map', {
+      center: [60.45, 22.26],
+      zoom: 16,
+      accessToken: "pk.eyJ1IjoibXl0dXJrdW1lbW9yaWVzIiwiYSI6ImNrNXhuZjdjMjBramMzbm54YWNjZWsweDQifQ.VSBHa6HkpaJfywOHhEjgbA", // Not being used
+      layers: [
+        L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXl0dXJrdW1lbW9yaWVzIiwiYSI6ImNrNXhuZjdjMjBramMzbm54YWNjZWsweDQifQ.VSBHa6HkpaJfywOHhEjgbA', {
+          attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }),
+      ]
+    });
+    this.marker = L.marker([60.45, 22.26]).addTo(this.map);
   }
   render() {
     return (
-      <Map center={this.state.center} zoom={13.5} layers="">
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-      </Map>
+      <div id='map'></div>
     )
   }
 }
