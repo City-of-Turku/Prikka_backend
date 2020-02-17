1db554d6bc746f635e94f3d7bc172c6188735bbe
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-02-17 10:54:48 +0200

    remove useless functions from login, format whole file -> add missing semicolons, etc. Should be more readable now

--- routes/login.js
+++ routes/login.js
@@ -5,83 +5,72 @@
  *
  *  (post) : '/'
  */
-const express = require('express')
-const passport = require('passport')
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn(
-  '/api/auth-management/login',
-)
+const express = require('express');
+const passport = require('passport');
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/api/auth-management/login');
 
-const loginRouter = express.Router()
+const loginRouter = express.Router();
 
-// login page
-loginRouter.get('/', function(req, res) {
-  //res.render('login')
-  res.send()
-})
+// // login page
+// loginRouter.get('/', function(req, res) {
+//   //res.render('login')
+//   res.send()
+// })
 
 // authentication via google
-loginRouter.get(
-  '/google',
-  passport.authenticate('google', { scope: ['profile'] }),
-)
-
-// loginRouter.get('/google-return', passport.authenticate('google', { failureRedirect: '/error' }), function(req, res) {
-// });
+loginRouter.get('/google', passport.authenticate('google', { scope: [ 'profile' ] }));
 
 loginRouter.get(
-  '/google-return',
-  passport.authenticate('google', {
-    failureRedirect: '/login',
-  }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    console.log('callback')
-    res.redirect('/api/auth-management/login/profile')
-  },
-)
+	'/google-return',
+	passport.authenticate('google', {
+		failureRedirect: '/login'
+	}),
+	function(req, res) {
+		// Successful authentication, redirect home.
+		console.log('callback');
+		res.redirect('/api/auth-management/login/profile');
+	}
+);
 // authentication via facebook
-loginRouter.get(
-  '/facebook',
-  passport.authenticate('facebook'),
-)
+loginRouter.get('/facebook', passport.authenticate('facebook'));
 
 loginRouter.get(
-  '/facebook-return',
-  passport.authenticate('facebook', {
-    failureRedirect: '/error',
-  }),
-  function(req, res) {
-    res.redirect('/api/auth-management/login/profile')
-  },
-)
+	'/facebook-return',
+	passport.authenticate('facebook', {
+		failureRedirect: '/login'
+	}),
+	function(req, res) {
+		res.redirect('/api/auth-management/login/profile');
+	}
+);
 
 // move this someplace else
 loginRouter.get('/profile', ensureLoggedIn, (req, res) => {
-  //res.render('profile', { user: req.user.dataValues })
-  res.send()
-})
+	//res.render('profile', { user: req.user.dataValues })
+	res.send();
+});
 // debug remove later please
 loginRouter.get('/secret', ensureLoggedIn, (req, res) => {
-  //res.render('secret')
-  res.send()
-})
+	//res.render('secret')
+	res.send();
+});
 // logout --- deauthenticate
 loginRouter.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
+	req.logout();
+	res.redirect('/');
+});
 
 loginRouter.get('/login', (res, req) => {
-  //res.render('login')
-  res.send()
-})
+	//res.render('login')
+	res.send();
+});
 
 loginRouter.post(
-  '/',
-  passport.authenticate('local-signin', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-  }),
-)
+	'/',
+	passport.authenticate('local-signin', {
+		successRedirect: '/',
+		failureRedirect: '/login'
+	})
+);
 
-module.exports = loginRouter
+module.exports = loginRouter;
