110805dd0e3583c557f02df6bb9fa9c03a613fd1
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-02-25 15:45:22 +0200

    JWT login, this might be useful. Instead of handling the sessions on the server, we have tokens that verify the users identify

--- routes/login.js
+++ routes/login.js
@@ -5,11 +5,12 @@
  *
  *  (post) : '/'
  */
+// const env = require('dotenv/config');
 const express = require('express');
 const passport = require('passport');
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn(
-    '/api/auth-management/login'
-);
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/api/auth-management/login');
+const jwt = require('jsonwebtoken');
+var _ = require('lodash');
 
 const loginRouter = express.Router();
 
@@ -20,62 +21,70 @@ const loginRouter = express.Router();
 // })
 
 // authentication via google
-loginRouter.get(
-    '/google',
-    passport.authenticate('google', { scope: ['profile'] })
-);
+loginRouter.get('/google', passport.authenticate('google', { scope: [ 'profile' ] }));
 
 loginRouter.get(
-    '/google-return',
-    passport.authenticate('google', {
-        failureRedirect: '/login',
-    }),
-    function(req, res) {
-        // Successful authentication, redirect home.
-        console.log('callback');
-        res.redirect('/api/auth-management/login/profile');
-    }
+	'/google-return',
+	passport.authenticate('google', {
+		failureRedirect: '/'
+	}),
+	function(req, res) {
+		// Successful authentication, redirect home.
+		console.log('callback');
+		res.redirect('/api/auth-management/login/profile');
+	}
 );
 // authentication via facebook
 loginRouter.get('/facebook', passport.authenticate('facebook'));
 
 loginRouter.get(
-    '/facebook-return',
-    passport.authenticate('facebook', {
-        failureRedirect: '/login',
-    }),
-    function(req, res) {
-        res.redirect('/api/auth-management/login/profile');
-    }
+	'/facebook-return',
+	passport.authenticate('facebook', {
+		failureRedirect: '/'
+	}),
+	function(req, res) {
+		res.redirect('/api/auth-management/login/profile');
+	}
 );
 
 // move this someplace else
-loginRouter.get('/profile', ensureLoggedIn, (req, res) => {
-    //res.render('profile', { user: req.user.dataValues })
-    res.send();
+loginRouter.get('/profile', (req, res) => {
+	//res.render('profile', { user: req.user.dataValues })
+	res.send();
 });
 // debug remove later please
-loginRouter.get('/secret', ensureLoggedIn, (req, res) => {
-    //res.render('secret')
-    res.send();
-});
+// loginRouter.get('/secret', (req, res, next) => {
+// 	//res.render('secret')
+// 	res.send("secret shit");
+// });
 // logout --- deauthenticate
 loginRouter.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
 
-loginRouter.get('/login', (res, req) => {
-    //res.render('login')
-    res.send();
+loginRouter.get('/', (res, req) => {
+	//res.render('login')
+	res.send();
 });
 
-loginRouter.post(
-    '/',
-    passport.authenticate('local-signin', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-    })
-);
+loginRouter.post('/', function(req, res, next) {
+	passport.authenticate('local-signin', { session: false }, (err, user, info) => {
+        user = _.pick(user, ['username']); // pick only the username from user object
+		if (err || _.isEmpty(user)) {
+			return res.status(400).json({
+				message: 'Something is not right',
+				user: user
+			});
+        }
+        req.login(user, {session: false}, (err) => {
+            if (err) {
+                res.send(err);
+            }
+            const token = jwt.sign(user['username'], process.env['JWT_SECRET']);
+            return res.json({user, token});
+        });
+	})(req, res);
+});
 
 module.exports = loginRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const _ = require('lodash');
 const usersRouter = express.Router();
 
 /* GET users listing. */
 usersRouter.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+	res.send('respond with a resource');
+});
+
+usersRouter.get('/profile', function(req, res, next) {
+	res.send(_.omit(req.user, [ 'passwordhash', 'googleId', 'facebookId' ]));
 });
 
 module.exports = usersRouter;
