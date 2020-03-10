3154a7adccb6d833efe226000d0799ffa6d85111
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-03-10 16:42:53 +0200

    include check for case when there is no token provided

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,7 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-    let token = req.headers.authorization.split(' ')[1];
+    let token = req.headers.authorization;
+    console.log(token)
+    if (typeof(token) === 'undefined') {
+        return res.status(400).json({
+            message: 'No Token, You shit',
+            token: token,
+        });
+    }
+    token = token.split(' ')[1];
     console.log(token)
     jwt.verify(token, process.env['JWT_SECRET'], function(err, decoded) {
         if (err) {
