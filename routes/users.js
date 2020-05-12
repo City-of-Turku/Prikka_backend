1e6b3573ba8057d2ae8d2f0486ef7fd1815e3aea
Robert Sundell <robert@weasel.fi>
2020-05-12 09:41:23 +0300

    Fixed a semicolon

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const logger = require('../config/winston');
 /* GET user profile. */
 usersRouter.get('/user', function (req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
-  logger.info(`Sent user profile to client. user: ${userProfile.id}`)
+  logger.info(`Sent user profile to client. user: ${userProfile.id}`);
   res.status(HttpStatus.OK).json({
     message: 'user profile',
     user: userProfile
