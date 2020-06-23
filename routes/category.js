ed5fb2ce8bb6d15e594e3e9430635f3e5ffa1805
Anders Vikstrand <anders@weasel.fi>
2020-06-23 16:25:58 +0300

    Frontend: Admin config page changes. New layout, edit / delete buttons added for existing categories, add new category button added, localization tags added / changed. Also a lot of changes to click handling, state objects. Backend: Api endpoint fix ( update category ).

--- routes/category.js
+++ routes/category.js
@@ -12,7 +12,7 @@ const categoryRouter = express.Router();
  */
 categoryRouter.get('/categories', function(req, res) {
     Category.findAll({
-        attributes: ['name', 'description', 'id'],
+        attributes: [ 'id', 'nameFI', 'descriptionFI', 'nameSV','descriptionSV', 'nameEN', 'descriptionEN'],
     })
         .then(categories => {
             logger.info(`List of categories sent - ${req.originalUrl} - ${req.method} - ${req.ip}`)
